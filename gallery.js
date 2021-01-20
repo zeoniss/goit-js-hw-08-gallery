7113cde60ae57e24b0cde36591c7c1f3c5b7bb98
Vladimir Kriukov <zeoniss28@gmail.com>
2021-01-20 20:38:45 +0200

    fix

--- gallery.js
+++ gallery.js
@@ -1,17 +1,16 @@
-import galleryItems from './gallery-items.js'
-const modalImgElem = document.querySelector('.lightbox_image')
-const closeBtnElem = document.querySelector('.lightbox_button')
-const galleryElem = document.querySelector('.js-gallery')
-const modalElem = document.querySelector('.js-lightbox')
-const overlayElem = document.querySelector('.lightbox_overlay')
+import galleryItems from './gallery-items.js';
+const modalImgElem = document.querySelector('.lightbox_image');
+const closeBtnElem = document.querySelector('.lightbox_button');
+const galleryElem = document.querySelector('.js-gallery');
+const modalElem = document.querySelector('.js-lightbox');
+const overlayElem = document.querySelector('.lightbox_overlay');
+const galleryListElem = createGalleryList(galleryItems);
 
-const galleryListElem = createGalleryList(galleryItems)
+galleryElem.insertAdjacentHTML('beforeend', galleryListElem);
 
-galleryElem.insertAdjacentHTML('beforeend', galleryListElem)
-
-galleryElem.addEventListener('click', onOpenModal)
-closeBtnElem.addEventListener('click', onCloseModal)
-overlayElem.addEventListener('click', onCloseModalOnOverlay)
+galleryElem.addEventListener('click', onOpenModal);
+closeBtnElem.addEventListener('click', onCloseModal);
+overlayElem.addEventListener('click', onCloseModalOnOverlay);
 
 function createGalleryList(img) {
     return img
@@ -28,38 +27,38 @@ function createGalleryList(img) {
         alt="${description}"
       />
     </a>
-  </li>`
+  </li>`;
         })
-        .join('')
+        .join('');
 }
 
 function onOpenModal(event) {
-    event.preventDefault()
-        // Проверяем тип узла, если не изображение - то мы выходим из функции
+    event.preventDefault();
+    // Проверяем тип узла, если не изображение - то мы выходим из функции
     if (event.target.nodeName !== 'IMG') {
-        return
+        return;
     }
-    document.addEventListener('keydown', onCloseModalByEsc)
+    document.addEventListener('keydown', onCloseModalByEsc);
 
-    modalElem.classList.add('is-open')
-    modalImgElem.src = event.target.dataset.source
-    modalImgElem.alt = event.target.alt
+    modalElem.classList.add('is-open');
+    modalImgElem.src = event.target.dataset.source;
+    modalImgElem.alt = event.target.alt;
 }
 
 function onCloseModal(event) {
-    document.removeEventListener('keydown', onCloseModalByEsc)
-    modalElem.classList.remove('is-open')
-    modalImgElem.src = ''
+    document.removeEventListener('keydown', onCloseModalByEsc);
+    modalElem.classList.remove('is-open');
+    modalImgElem.src = '';
 }
 
 function onCloseModalByEsc(event) {
     if (event.code === 'Escape') {
-        onCloseModal()
+        onCloseModal();
     }
 }
 
 function onCloseModalOnOverlay(event) {
     if (event.currentTarget === event.target) {
-        onCloseModal()
+        onCloseModal();
     }
 }
